b7a2fe9e5b89b2ab539d409bfa97392a0f8f7909
daniel <danielserva@gmail.com>
2025-02-26 21:51:13 +0100

    generating random id

--- identity/generation.py
+++ identity/generation.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
-
+import random
 from .constants import ID_CHARACTERS
 
-
+"""
+Generates random 7 character human-readable ID
+"""
 def generate():
-    return None
+    generated_id = ''
+    for _ in range(7):
+        generated_id = generated_id + random.choice(ID_CHARACTERS)
+    return generated_id
